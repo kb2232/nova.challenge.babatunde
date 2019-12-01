954875df07ee5c98a2598790d713faa9f9916d38
kunle babatunde <kb2232@nyu.edu>
2019-12-01 07:39:47 -0500

    added load more data button...fix pending transition state

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,9 +10,9 @@ export const testAction = () =>{
 }
 
 // fetch record
-export const fetchRecords = ()=>{
+export const fetchRecords = (countN)=>{
   return async function(dispatch){
-    const response = await JSONplaceholder.get('/api/requests?count=10&cursor=2010-11-12T02:18:22.094Z');
+    const response = await JSONplaceholder.get(`/api/requests?count=${countN}&cursor=2010-11-12T02:18:22.094Z`);
     dispatch({
         type:'FETCH_RECORDS',
         payload:response.data
--- src/components/recordlist/listRecord.js
+++ src/components/recordlist/listRecord.js
@@ -4,8 +4,14 @@ import {connect} from 'react-redux';
 import {fetchRecords} from '../../actions';
 
 class RecordList extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      countN:10
+    }
+  }
   componentDidMount(){
-    this.props.fetchRecords();
+    this.props.fetchRecords(this.state.countN);
   }
   renderList(){
     if(this.props.records.reports){
@@ -28,9 +34,24 @@ class RecordList extends Component{
     }
   }
   render(){
+    const {ButtonContainer} = styles;
     return(
-      <div className="row_c_wrap">
-       {this.renderList()}
+      <div>
+        <div className="row_c_wrap">
+          {this.renderList()}
+        </div>
+        <div className="button_c_c">
+          <button 
+            onClick={()=>{
+              this.setState({countN:this.state.countN+5},()=>{
+                this.props.fetchRecords(this.state.countN);
+              });
+            }}
+            style={ButtonContainer}
+          >
+          LOAD MORE DATA
+          </button>
+        </div>
       </div>
     )
   }
@@ -40,4 +61,18 @@ const mapstate2props = state =>{
     records:state.recordReducer
   }
 }
+
+const styles = {
+  ButtonContainer:{
+    width:"200px",
+    height:"40px",
+    marginLeft:'10px',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#3D68A3',
+    cursor:'pointer'
+  }
+}
+
 export default connect(mapstate2props,{fetchRecords})(RecordList);
